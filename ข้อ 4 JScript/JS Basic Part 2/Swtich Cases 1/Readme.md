9e4e88bb3f5a4f38e5b25b24fd6128ec9f078c12
Prakasit993 <prakasit993@gmail.com>
2023-06-09 13:58:20 +0700

    Make new file

--- ข้อ 4 JScript/JS Basic Part 2/Swtich Cases 1/Readme.md
+++ ข้อ 4 JScript/JS Basic Part 2/Swtich Cases 1/Readme.md
@@ -5,3 +5,30 @@
 ---
 
 #### โจทย์การบ้านที่ทำ
+
+แปลงนี้ Code ดังกล่าวเป็น if - else statement
+
+> switch (browser) {
+>
+> case 'Edge':
+>
+> alert( "You've got the Edge!" );
+>
+> break;
+>
+> case 'Chrome':
+>
+> case 'Firefox':
+>
+> case 'Safari':
+>
+> case 'Opera':
+>
+> alert( 'Okay we support these browsers too' );
+>
+> break;
+>
+> default:
+> alert( 'We hope that this page looks ok!' );
+>
+> }
---
+++ ข้อ 4 JScript/JS Basic Part 2/Swtich Cases 1/Script.js
@@ -0,0 +1,15 @@
+let browser = prompt("Try browser");
+
+if (browser == Edge) {
+  alert("You have got the Edge!");
+} else if (browser == Chrome) {
+  alert("Okay we support these browsers too");
+} else if (browser == Firefox) {
+  alert("Okay we support these browsers too");
+} else if (browser == Safari) {
+  alert("Okay we support these browsers too");
+} else if (browser == Opera) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
